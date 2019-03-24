cb3a1894c561b7bb10210f6479798d1172bb1e20
marshallpowell97 <marshallpowell97@gmail.com>
2019-03-24 15:54:04 -0400

    add more functionality to gamepad.py

--- src/gamepad.py
+++ src/gamepad.py
@@ -5,35 +5,61 @@ from geometry_msgs.msg import Twist
 from inputs import devices, get_gamepad
 from geometry_msgs.msg import Twist
 
+
+
 def getButton():
     direction = "k"
     events = get_gamepad()
     for event in events:
        if(event.code == "ABS_X" and event.state == 0):
           direction = "j"
-       elif(event.code == "ABS_X" and event.state == 255):
+          return direction
+       if(event.code == "ABS_X" and event.state == 255):
           direction = "l"
-       elif(event.code == "ABS_Y" and event.state == 0):
+          return direction
+       if(event.code == "ABS_Y" and event.state == 0):
           direction = "i"
-       elif(event.code == "ABS_Y" and event.state == 255):
+          return direction
+       if(event.code == "ABS_Y" and event.state == 255):
           direction = ","
-       elif(event.code == "BTN_PINKIE" and event.state == 1):
+          return direction
+       if(event.code == "BTN_PINKIE" and event.state == 1):
           direction = "L"
-       elif(event.code == "BTN_TOP2" and event.state == 1):
+          return direction
+       if(event.code == "BTN_TOP2" and event.state == 1):
           direction = "J"
-    return direction
+          return direction
+
+def gamepad():
+   rospy.init_node('teleop_twist_gamepad')
+   pub = rospy.Publisher('cmd_vel', Twist, queue_size = 10)
+   twist = Twist()
+   if(getButton() == "i"):
+      twist.linear.x = 0.5
+      pub.publish(twist)
+   if(getButton() == ","):
+      twist.linear.x = -0.5
+      pub.publish(twist)
+   if(getButton() == "j"):
+      twist.angular.z = 1
+      pub.publish(twist)
+   if(getButton() == "l"):
+      twist.angular.z = -1
+      pub.publish(twist)
+   if(getButton() == "k"):
+      twist.angular.z = 0
+#      pub.publish(twist)
+
+#   print(getButton())
+#   rospy.spin()
+
 
 if __name__ == '__main__':
     try:
-        pub = rospy.Publisher('cmd_vel', Twist, queue_size = 2)
-        rospy.init_node('gamepad_control')
-        twist = Twist()
-        if(getButton() == "i"):
-           twist.linear.y = 0.5
-           pub.publish(twist)
+        while(1):
+           gamepad()
 
     except rospy.ROSInterruptException:
         pass
 
-#rospy.spin()
 
