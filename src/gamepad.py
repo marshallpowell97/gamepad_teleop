1b181af2f0341dabd45d52ddaa72b2db61327ec0
marshallpowell97 <marshallpowell97@gmail.com>
2019-03-24 16:10:52 -0400

    fix gamepad.py, clean up code

--- src/gamepad.py
+++ src/gamepad.py
@@ -13,45 +13,35 @@ def getButton():
     for event in events:
        if(event.code == "ABS_X" and event.state == 0):
           direction = "j"
-          return direction
        if(event.code == "ABS_X" and event.state == 255):
           direction = "l"
-          return direction
        if(event.code == "ABS_Y" and event.state == 0):
           direction = "i"
-          return direction
        if(event.code == "ABS_Y" and event.state == 255):
           direction = ","
-          return direction
        if(event.code == "BTN_PINKIE" and event.state == 1):
           direction = "L"
-          return direction
        if(event.code == "BTN_TOP2" and event.state == 1):
           direction = "J"
-          return direction
+       return direction
 
 def gamepad():
    rospy.init_node('teleop_twist_gamepad')
    pub = rospy.Publisher('cmd_vel', Twist, queue_size = 10)
    twist = Twist()
-   if(getButton() == "i"):
+   button = getButton()
+   if(button == "i"):
       twist.linear.x = 0.5
       pub.publish(twist)
-   if(getButton() == ","):
+   if(button == ","):
       twist.linear.x = -0.5
       pub.publish(twist)
-   if(getButton() == "j"):
+   if(button == "j"):
       twist.angular.z = 1
       pub.publish(twist)
-   if(getButton() == "l"):
+   if(button == "l"):
       twist.angular.z = -1
       pub.publish(twist)
-   if(getButton() == "k"):
-      twist.angular.z = 0
-#      pub.publish(twist)
-
-#   print(getButton())
-#   rospy.spin()
 
 
 if __name__ == '__main__':
